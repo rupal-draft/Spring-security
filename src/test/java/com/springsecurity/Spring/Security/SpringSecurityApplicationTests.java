3566c57cea1db559a0374018707c703e27854b41
Rupal Paul <rupalpaulgspr@gmail.com>
2024-10-25 12:54:19 -0700

    Initial Setup

---
+++ src/test/java/com/springsecurity/Spring/Security/SpringSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springsecurity.Spring.Security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
